e1937cc972728b9f8e5302cefa2ce2d608c6f6d4
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-14 16:16:13 -0700

    test

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - the program will assign a random number to the variable n
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 10)
+	{
+		putchar (c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
