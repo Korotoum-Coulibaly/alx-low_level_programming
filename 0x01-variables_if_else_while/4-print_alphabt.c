f92133fddd96405048f23c9ed8ff304afe844997
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-14 15:48:10 -0700

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - the program will assign a random number to the variable n
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char ligne;
+
+	c = 'a';
+	ligne = '\n';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar (c);
+		}
+		c++;
+	}
+	putchar (ligne);
+	return (0);
+}
