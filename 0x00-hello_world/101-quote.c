64543b846a65ac27698c756e1a48a77196ab081d
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-13 18:32:26 -0700

    print without man 3 printf and man 3 puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Program that prints exactly message
+ * \"and that piece of art is useful\" - Dora Korpar, 2015-10-19,
+ *
+ * Return: 1 (Sucess)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
+	return (1);
+}
