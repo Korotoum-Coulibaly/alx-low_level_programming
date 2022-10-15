5a42df9c3990b8a9eb511aa97fd82c281566eedc
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-14 17:40:48 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - the program will assign a random number to the variable n
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	char ligne;
+
+	c = 0;
+	ligne = '\n';
+	while (c <= 15)
+	{
+		if (c < 10)
+			putchar ('0' + c);
+		if (c >= 10 && c <= 15)
+			putchar ((c - 10)["ABCDEF"]);
+		c++;
+	}
+	putchar (ligne);
+	return (0);
+}
