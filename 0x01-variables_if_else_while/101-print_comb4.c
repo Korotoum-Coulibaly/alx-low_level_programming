1b334d4c91c699440f925d63c57335d4b91bf892
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-14 19:59:44 -0700

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - the program will assign a random number to the variable n
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstnumber, secondnumber, thirdnumber;
+
+	for (firstnumber = 0; firstnumber < 8 ; firstnumber++)
+	{
+		for (secondnumber = 1; secondnumber < 9; secondnumber++)
+		{
+			for (thirdnumber = 2; thirdnumber <= 9; thirdnumber++)
+			{
+				if (firstnumber < thirdnumber && thirdnumber > secondnumber && (firstnumber != secondnumber) != thirdnumber)
+				{
+					putchar ('0' + firstnumber);
+					putchar ('0' + secondnumber);
+					putchar ('0' + thirdnumber);
+					if (firstnumber != 7 || secondnumber != 8 || thirdnumber != 9)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}	
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
