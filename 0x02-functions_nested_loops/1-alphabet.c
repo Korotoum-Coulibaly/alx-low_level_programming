bc27d8d68170a711313feb03e45c18933e863c70
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 13:30:57 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * main - programm with function print_alphabet
+ * that print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
