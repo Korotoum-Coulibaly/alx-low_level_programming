4d159cfd02d06d8fb4c09b318f0d1fd1d3fb819f
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 13:17:48 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
 #include "main.h"
+
+/**
+ * main - program that prints putchar,followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char info;
-
-	info = 'p';
-	putchar (info);
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,1 @@
 #include <stdio.h>
-
-/**
- * main - Program that prints putchar followed by a new line
- *
- * Return: Always 0 (Success)
- */
