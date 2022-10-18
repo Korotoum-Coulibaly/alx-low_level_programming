4bc9ad2f9b033d5595f287890b83ef8cc7a8cd12
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 14:43:50 -0700

    Task2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,10 +10,24 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
 		putchar (letter);
-	}
-		putchar ('\n');
+	putchar ('\n');
 }
 
+/**
+ * print_alphabet_x10 - this function prints 10 times
+ *  the alphabet,in lowercase,followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar (letter);
+		putchar ('\n');
+	}
+}
 #endif /* _MAIN_H_ */
