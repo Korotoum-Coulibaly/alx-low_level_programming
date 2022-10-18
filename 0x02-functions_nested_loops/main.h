25865621eb7b1ccc2037713158bd9b351b0f783b
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 16:24:49 -0700

    task 4

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -47,4 +47,19 @@ int _islower(int c)
 		return (0);
 }
 
+/**
+ * _isalpha - checks alphabetic character
+ * @c: is the information that checks
+ *
+ * Return: 1 if c is a letter,lowercase or uppercase
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
+
 #endif /* _MAIN_H_ */
