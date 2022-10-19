fd109ab4808dfe9946f10a6ec274501fc8bfd152
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 20:37:19 -0700

    task 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -209,4 +209,18 @@ void times_table(void)
 	}
 }
 
+/**
+ * add - that adds two integers and returns the result
+ * @n: first number
+ * @m: second number
+ *
+ * Return: $result
+ */
+int add(int n,int m)
+{
+	int result;
+
+	result = n + m;
+	return (result);
+}
 #endif /* _MAIN_H_ */
