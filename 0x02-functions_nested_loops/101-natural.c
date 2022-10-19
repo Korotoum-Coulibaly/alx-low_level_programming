ae406745858c235d059a4c06e3c6612b14140340
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-19 09:36:16 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - that computes and prints the sum
+ * off all the multiples of 3 or 5 below 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count, sum;
+	
+	sum = 0;
+	for (count = 0; count < 1024; count++)
+	{
+		if ((count % 3) == 0 || (count % 5) == 0)
+		{
+			sum = sum + count;
+		}
+	}
+	printf ("%d \n",sum);
+	return (0);
+}
