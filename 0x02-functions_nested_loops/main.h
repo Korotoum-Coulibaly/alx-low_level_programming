e9dfb3a3abe5c7f93691a42bd1321079dd9789c3
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 18:59:50 -0700

    task 8

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -135,24 +135,46 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a < 2; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		if (a != 2)
 		{
-			for (c = 0; c <= 5; c++)
+			for (b = 0; b <= 9; b++)
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					putchar ('0' + a);
-					putchar ('0' + b);
-					putchar (':');
-					putchar ('0' + c);
-					putchar ('0' + d);
-					putchar ('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						putchar ('0' + a);
+						putchar ('0' + b);
+						putchar (':');
+						putchar ('0' + c);
+						putchar ('0' + d);
+						putchar ('\n');
+					}
 				}
 			}
 		}
-	}
+		else
+		{
+			for (b = 0; b <= 3; b++)
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for (d = 0; d <= 9; d++)
+					{
+						putchar ('0' + a);
+						putchar	('0' + b);
+						putchar (':');
+						putchar ('0' + c);
+						putchar ('0' + d);
+						putchar ('\n');
+					}
+				}
+			}
+		}
+	}	
+
 }
 
 #endif /* _MAIN_H_ */
