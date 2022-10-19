78b69cff7c5c0ba6bc7d529f284f34a47e83e972
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-18 21:20:02 -0700

    task 11

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -251,8 +251,8 @@ void print_to_98(int n)
 	}
 	for (p = n; p > 98; p--)
 	{
+		_putchar ('0' + p / 20);
 		_putchar ('0' + p / 10);
-		_putchar ('0' + p / 100);
 		_putchar ('0' + p % 10);
 		if (p != 98)
 		{
