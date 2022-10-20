3c21ec38f18d9d73f6b4cbc9b4378c77413d9393
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-20 16:13:55 -0700

    task 3 2 conditions is okay

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -13,9 +13,9 @@ int main(void)
 	int day;
 	int year;
 
-	month = 0;
-	day = 0;
-	year = 2000;
+	month = 00;
+	day = 01;
+	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 	day = convert_day(month, day);
