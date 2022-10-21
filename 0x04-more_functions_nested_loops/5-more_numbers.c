a0e6256eb3f3ff5f3d4b0f5f6a006a8cf410423c
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-20 18:00:16 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *more_numbers - that prints 10 times the numbers,from 0 to 14
+ */
+void more_numbers(void)
+{
+	int c, b;
+
+	for (b = 0; b < 10; b++)
+	{	
+		for (c = 0; c < 15; c++)
+		{
+			_putchar ('0' + c);
+		}
+	}
+	_putchar ('\n');
+}
