78d1d0d4746ee30bc4da87db9a921ea465765d20
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-20 17:45:58 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_numbers - that print the numbers from 0 to 9
+ */
+int _isupper(int c)
+{
+	for (c = 0; c < 10; c++)
+	{
+		_putchar ('0' + c);
+	}
+	_putchar ('\n');
+}
