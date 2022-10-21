3e8c975f4141fae1427c0d7a8a250570ca3b8ab9
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-20 17:39:47 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - that multiples two integers
+ *@a: first number
+ *@b: second number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
