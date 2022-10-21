07e176d942562260892fcf8bbe969c92619b9ca1
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-20 19:53:16 -0700

    task 7 update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  *print_diagonal - that draws a diagonal line on the terminal
- *@n : the number of times the character \ should be printed
+ *@n : the number  of times the character \ should be printed
  */
 void print_diagonal(int n)
 {
