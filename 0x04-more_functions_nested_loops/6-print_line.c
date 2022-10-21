f5fc84a049b6e29e4e45c8610b3c4f8454290e69
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-20 18:35:25 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line: that draws a straight line in the terminal
+ */
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		if (n > 0)
+			_putchar ('_');
+		else
+			_putchar ('\n';
+	}
+	_putchar ('\n');
+}
