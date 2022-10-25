9b015948e73054534c8468ccd258b5c5d85eb220
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-10-24 17:51:38 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,9 +13,9 @@ void rev_string(char *s)
 	a = 0;
 	for (i = n - 1; i >= 0; i--)
 	{
-		sauvegarde = s[i];
-		s[i] = s[a];
-		s[a] = sauvegarde;
+		sauvegarde = *s[i];
+		*s[i] = *s[a];
+		*s[a] = sauvegarde;
 		a++;
 	}
 	_putchar('\n');
