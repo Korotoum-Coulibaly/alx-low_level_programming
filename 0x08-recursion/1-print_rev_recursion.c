3f2de63f5aad89b9fcccc4724eca86f60e76973c
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-02 15:49:15 -0700

    task 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - that prints a string
+ * @s: text
+ */
+void _print_rev_recursion(char *s)
+{
+	
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s - 1);
+	}
+	else
+		_putchar('\n');
+}
