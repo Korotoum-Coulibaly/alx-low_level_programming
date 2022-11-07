cdb68dabf422cf5d15c3ffc35b995cbbae9f4514
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-07 01:44:57 -0800

    initialize

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: first parameter
+ * @b: second parameter
+ * @n: the first n bytes
+ *
+ * Return: a pointer to the memory area s
+ * By Coulby Dev
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdlib.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* _MAIN_H */
