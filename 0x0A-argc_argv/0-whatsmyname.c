b7d8c2a4b23fd0293dd7d816e8c0ccff2dfa6b65
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-09 15:50:49 -0800

    task 0 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *main - program that prints its name,followed by a new line
+ *
+ * Return: argc
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *(argv));
+	return (argc);
+}
