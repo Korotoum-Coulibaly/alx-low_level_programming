0082a819b494bf7907130f849eeafb3ddb94bf68
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-08 17:22:30 -0800

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,14 +12,14 @@
 int **alloc_grid(int width, int height)
 {
 
-	int **memory;
+	int **memory = NULL;
 	int i, j;
 
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
 		{
-			*memory = (int **) malloc(sizeof(int) * (width * height));
+			*memory = (int *) malloc(sizeof(int) * (width * height));
 			**(memory + ((i * width) * j)) = 0;
 		}
 	}
