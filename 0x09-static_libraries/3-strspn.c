fd62ad824ec1476bb5b4fcfac16d8d94fb288021
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-09 13:50:59 -0800

    task 0 0x09

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -9,7 +9,7 @@
  * Return: the number of bytes in the initial segment 
  * of s which consist only of bytes from accept
  */
-unisgned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-
+	return(0);
 }
