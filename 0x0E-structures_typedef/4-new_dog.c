aa12154c0d45b8ba526d9cca9df7e8189352a7a2
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-15 14:06:34 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,17 +12,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
+	int i;
 	dog_t *my_dog;
 
+	/*gestion d'erreur malloc*/
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-
+	/*enregistrement du nouveau chien*/
 	(*my_dog).name = name;
 	(*my_dog).age = age;
 	(*my_dog).owner = owner;
-	
+	/*suppression de l'espace alloué si on a pas owner ou name*/
+	if ((*my_dog).owner == NULL || (*my_dog).name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	/*enregistrement d'une copie en mémoire*/
+	for (i = 0; i < (int) sizeof(dog_t); i++)
+	{
+		*(name + i) = *name;
+		*(owner + i) = *owner;
+	}
 	return (my_dog);
 	free(my_dog);
 }
