2610440ecf7ff0af292ac469fafa9ec97bc9aef1
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-15 07:26:18 -0800

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,21 +1,2 @@
-#ifndef _MACRO_
-#include <stdio.h>
 #define ABS(x)\
-	if (x > 0) {(x = x)} else {(x = -x)}
-
-/**
- * abs - function that computes the absolute value of a number x
- * @x: value
- *
- * Return: result
- 
-int ABS(int x)
-{
-	int result;
-
-	result = ABS(x);
-	return (result);
-}
-*/
-
-#endif /*_MACRO_ */
+	((x) > 0 ? (x) : (-x))
