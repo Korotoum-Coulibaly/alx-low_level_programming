313b672a0224cf1583cab06cd2a8ec6b5194f01c
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-15 13:39:09 -0800

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: information
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+		return (NULL);
+	else
+	{
+		(*my_dog).name = name;
+		(*my_dog).age = age;
+		(*my_dog).owner = owner;
+	}
+	return (my_dog);
+}
