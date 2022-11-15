00fa3c7ce57b199184db0a5b6edbed926be9bb2b
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-15 13:46:27 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,11 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-	else
-	{
-		(*my_dog).name = name;
-		(*my_dog).age = age;
-		(*my_dog).owner = owner;
-	}
+
+	(*my_dog).name = name;
+	(*my_dog).age = age;
+	(*my_dog).owner = owner;
 	return (my_dog);
 }
