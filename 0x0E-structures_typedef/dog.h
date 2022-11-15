8d13400c019124f023d06e3120d3f8835a6437cd
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-15 08:36:58 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - that define Poppy
+ * @age: age
+ * @name: name
+ * @owner: owner
+ *
+ * Description: Define a new type struct dog in this file
+ */
+struct dog
+{
+	char name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H_ */
