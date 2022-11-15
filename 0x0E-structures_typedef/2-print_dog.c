c4f8bbf035a8facb7cfda97aac4da00ec521076b
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-15 09:39:42 -0800

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: new struct dog
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("Nothing\n");
+	else
+	{
+		if ((*d).name == NULL)
+		{
+			printf("Name: (nil)\n");
+			printf("Age: %d\n", (*d).age);
+			printf("Owner: %s\n", (*d).owner);
+		}
+		if ((*d).age == NULL)
+		{
+			printf("Name: %s\n", (*d).name);
+			printf("Age: (nil)");
+			printf("Owner: %s\n", (*d).owner);
+		}
+		if((*d).owner == NULL)
+		{
+			printf("Name: %s\n", (*d).name);
+			printf("Age: %d\n", (*d).age);
+			printf("Owner: (nil)");
+		}
+	}
+}
