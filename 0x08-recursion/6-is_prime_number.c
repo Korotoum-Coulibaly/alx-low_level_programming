c6faa8c8c851b69d80e8bcaad3479d5ed88a94a9
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-17 08:39:42 -0800

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that returns 1 if the input
+ * integer is a prime number, otherwise return 0
+ *@n: value
+
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	/*the first prime number is two*/
+	if (n == 2)
+		return (1);
+	/*prime is a function that search prime number*/
+	return (prime(2,n));
+
+}
+
+/**
+ * prime - function that search prime number
+ * @a: number
+ * @n: number that verified
+ *
+ * Return: 0 or 1
+ */
+int prime(int a, int n)
+{
+	if (n < 2)
+		return (0);
+	if (a > (n / 2))
+		return (1);
+	return (prime(a + 1, n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int factorial(int n);
 int _sqrt_recursion(int n);
+int _is_prime_number(int n);
 
 #endif /* _MAIN_H */
