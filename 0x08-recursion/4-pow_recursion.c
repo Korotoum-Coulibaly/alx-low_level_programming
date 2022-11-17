f66b33c510701d284daddbaa3d57d2a4b9ecc5d7
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-17 07:18:14 -0800

    task 5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,8 +11,12 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-
-	_pow_recursion(x, y - 1);
-	x = x * x;
+	if (y == 0)
+		return (1);
+	if (y > 0)
+	{
+		_pow_recursion(x, (y - 1));
+		x = x * x;
+	}
 	return (x);
 }
