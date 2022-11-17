e27e67191f33c1619c39345e218784f3683b8ae5
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-17 04:12:07 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,7 +12,9 @@ int _strlen_recursion(char *s)
 
 	if (*s)
 	{
-		count += _strlen_recursion(s + 1);
+		/*count += _strlen_recursion(s + 1);*/
+		count = _strlen_recursion(s);
+		_strlen_recursion(s + 1);
 		count++;
 	}
 	return (count);
