2003ad39a5c39af16ed606046b957886d4f1ad83
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-17 01:36:53 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,22 +12,39 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int compteur = 0;
-	int i, sizeA, j, sizeS;
+	unsigned int count = 0;
+	int i, sizeA, j, sizeS, k, status;
+	char *copy = malloc(sizeof(char));
 
 	sizeA = strlen(accept);
 	sizeS = strlen(s);
-
+	
+	/* browe array accept*/
 	for (i = 0; i < sizeA; i++)
 	{
+		/* browse array s or text enter*/
 		for (j = 0; j < sizeS; j++)
 		{
+			/*browse array copy to verified if element are not repeat*/
+			for (k = 0; k < sizeA; k++)
+			{
+				if ((* accept + i) == *(copy + k))
+					status = 1;
+				else
+					*(copy + i) = *(accept + i);
+			}
+
+			/*stop to browse array s if element repeat*/
+			if (status == 1)
+				break;
+			/*if element is not repeat increment count*/
 			if (*(accept + i) == *(s + j))
 			{
-				compteur++;
+				count++;
 				break;
 			}
 		}
 	}
-	return (compteur);
+	free(copy);
+	return (count);
 }
