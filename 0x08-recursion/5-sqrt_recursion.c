f64bdf11d6f5f899e1d681e23b8c9c60eeafcf7c
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-11-17 07:37:08 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square
+ * root of a number
+ * @n: number
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	n = _sqrt_recursion(n - 1) ^ 0.5;
+	return (n);
+}
