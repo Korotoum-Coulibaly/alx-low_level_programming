ec3ff799085eadd4a8b7c6520ff6d544ad76b460
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2022-12-08 10:44:47 -0800

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_string - function that sets the value of a 
+ * set_string - function that sets the value of a
  * pointer to a char
  * @s: first string
  * @to: second string
