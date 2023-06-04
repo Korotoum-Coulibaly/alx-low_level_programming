a20061dc377b4512e3e8428474f96ca62ce52d58
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-04 07:30:19 -0700

    add header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <math.h>
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) < (b) ? (b) : (a))
+
+/* Functions Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /* _SEARCH_ALGOS_H_ */
